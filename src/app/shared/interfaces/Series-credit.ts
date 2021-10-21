2ebe74a224f2ee41809849ebe79b2febc64c9e28
vignesh-v97 <vigneshbharathi34@gmail.com>
2021-10-21 13:02:00 +0530

    series cast view

---
+++ src/app/shared/interfaces/Series-credit.ts
@@ -0,0 +1,17 @@
+export interface Cast {
+ adult: boolean;
+ gender: number;
+ id: number;
+ known_for_department: string;
+ name: string;
+ original_name: string;
+ popularity: number;
+ profile_path: string;
+ character: string;
+ credit_id: string;
+ order: number;
+}
+
+export interface ISeriesCredit {
+ cast: Cast[];
+}
